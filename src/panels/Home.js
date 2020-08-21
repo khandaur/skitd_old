3d07ca947cac9c13966d3a21c325cfd262eb59ec
khandaur <andrey@khandaur.com>
2020-08-21 19:07:26 +0500

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -10,7 +10,7 @@ import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Участник конкурсов</PanelHeader>
+		<PanelHeader>Участники конкурсов</PanelHeader>
 		{fetchedUser &&
 		<Group title="User Data Fetched with VK Bridge">
 			<Cell
