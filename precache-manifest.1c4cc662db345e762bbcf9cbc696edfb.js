f0d667ee63ba5fda46acab3c52747e24d36f9da8
khandaur <andrey@khandaur.com>
2020-08-21 16:30:48 +0500

    Updates

---
+++ precache-manifest.1c4cc662db345e762bbcf9cbc696edfb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3964d6ccfe45be0668606000fdf46c43",
+    "url": "/skitd/index.html"
+  },
+  {
+    "revision": "76bc79e17f6c4f58a632",
+    "url": "/skitd/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "879124ec8f8506d9a239",
+    "url": "/skitd/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "76bc79e17f6c4f58a632",
+    "url": "/skitd/static/js/2.eea56a0f.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/skitd/static/js/2.eea56a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "879124ec8f8506d9a239",
+    "url": "/skitd/static/js/main.835ee606.chunk.js"
+  },
+  {
+    "revision": "620cc90152d39bed45b9",
+    "url": "/skitd/static/js/runtime-main.c2a357b0.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/skitd/static/media/persik.4e1ec840.png"
+  }
+]);
